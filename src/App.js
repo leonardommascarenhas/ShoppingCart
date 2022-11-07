2264be55382ec8f1d394f3da1283d5f9e0d3c8b7
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-07 14:58:55 -0300

    min-height

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./Modules/Header/Header";
+import ShoppingCart from "./Components/ShoppingCart";
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="h-screen w-screen flex flex-col item-center">
+      <ShoppingCart />
       <Header />
     </div>
   );
--- src/Modules/Header/Header.js
+++ src/Modules/Header/Header.js
@@ -1,9 +1,7 @@
-import ShoppingCart from "../../Components/ShoppingCart";
-
 const Header = () => {
   return (
-    <header className="w-screen flex">
-      <ShoppingCart />
+    <header className="w-screen flex justify-center items-center font-title shadow-lg bg-slate-300 min-h-[100px]">
+      <h1>SUDO</h1>
     </header>
   );
 };
