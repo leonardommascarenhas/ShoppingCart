8838d262ef1a266e93f00af17a14b8d684fca9c0
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-06 21:39:48 -0300

    first commit

---
+++ src/Components/ShoppingCart.js
@@ -0,0 +1,7 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+const ShoppingCart = () => {
+  return <span>{<AiOutlineShoppingCart size={28} />}</span>;
+};
+
+export default ShoppingCart;
