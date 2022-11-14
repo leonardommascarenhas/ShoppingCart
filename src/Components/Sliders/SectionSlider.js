6c2da48da76c400dd2e0d5d9f6d36f2c69e11941
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-14 12:25:44 -0300

    slide fix

--- src/Components/Sliders/SectionSlider.js
+++ src/Components/Sliders/SectionSlider.js
@@ -1,3 +1,7 @@
 import { Swiper, SwiperSlider } from "swiper";
-
 import "swiper/css";
+import { Navigation } from "swiper";
+
+const SectionSlider = () => {
+  <Swiper modules={[Navigation]} slidesPerView={4} spaceBetween={20} navigation></Swiper>;
+};
