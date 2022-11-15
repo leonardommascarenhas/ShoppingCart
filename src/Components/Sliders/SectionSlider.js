f9f988fb72688bc5224a0b2da642ef7634632ae7
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-14 23:57:37 -0300

    second slider test

--- src/Components/Sliders/SectionSlider.js
+++ src/Components/Sliders/SectionSlider.js
@@ -1,7 +1,35 @@
-import { Swiper, SwiperSlider } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation } from "swiper";
 
+import Card from "../Card";
+import img1 from "../../imgs/Black-shirts/Masculino/allef-vinicius-flFI_7Rs6Ik-unsplash.jpg";
 const SectionSlider = () => {
-  <Swiper modules={[Navigation]} slidesPerView={4} spaceBetween={20} navigation></Swiper>;
+  return (
+    <>
+      {
+        <Swiper modules={[Navigation]} slidesPerView={4} spaceBetween={20} navigation>
+          <SwiperSlide>
+            <Card itemImage={img1} itemDescription="Teste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card itemImage={img1} itemDescription="Teste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card itemImage={img1} itemDescription="Teste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card itemImage={img1} itemDescription="Teste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card itemImage={img1} itemDescription="Teste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card itemImage={img1} itemDescription="Teste" />
+          </SwiperSlide>
+        </Swiper>
+      }
+    </>
+  );
 };
+export default SectionSlider;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,10 @@
 import MainSlider from "../Components/Sliders/MainSlider";
+import SectionSlider from "../Components/Sliders/SectionSlider";
 function Home() {
   return (
     <>
       <MainSlider />
+      <SectionSlider />
     </>
   );
 }
