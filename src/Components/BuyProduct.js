b2ae8a72db394ba7b6979949bedad3482744207b
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-20 16:51:00 -0300

    filter display

--- src/Components/BuyProduct.js
+++ src/Components/BuyProduct.js
@@ -2,18 +2,16 @@ import { useParams } from "react-router-dom";
 import React from "react";
 import { AuthContext } from "../Providers/auth";
 
-const BuyProduct = ({}) => {
+const BuyProduct = () => {
   const clothes = React.useContext(AuthContext);
+
   let { product } = useParams();
-  return (
-    <div>
-      {product}
-      {console.log(clothes)}
-      {console.log(
-        "começar daqui, utilizar filter aqui para achar o produto e depois exibir na tela dinamicamente"
-      )}
-    </div>
-  );
+
+  const clickedCloth = clothes.filter((cloth) => {
+    return cloth.link == product;
+  });
+
+  return <div>{clickedCloth.map((cloth) => cloth.link)}</div>;
 };
 
 export default BuyProduct;
