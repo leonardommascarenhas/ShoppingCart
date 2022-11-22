cc0c8988fe4b93ed5741c2e1013a2e3a06ab942b
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-22 12:39:48 -0300

    mapping acessories array display

--- src/Pages/Acessorios.js
+++ src/Pages/Acessorios.js
@@ -1,19 +1,28 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Providers/auth";
 import Card from "../Components/Card";
+
+import { useNavigate } from "react-router-dom";
 const Acessorios = () => {
   const { acessorios } = React.useContext(AuthContext);
+  let navigate = useNavigate();
 
   return (
     <div>
-      {acessorios.map((acessorio) => {
-        <Card
-          productImage={acessorio.imageSrc}
-          productImageAlt={acessorio.alt}
-          productName={acessorio.name}
-          productPrice={acessorio.price}
-        />;
-      })}
+      {acessorios.map((acessorio) => (
+        <div
+          key={acessorio.id}
+          onClick={() => {
+            navigate(`/collection/${acessorio.link}`);
+          }}
+        >
+          <Card
+            productImage={acessorio.imageSrc}
+            productName={acessorio.name}
+            productPrice={acessorio.price}
+          />
+        </div>
+      ))}
     </div>
   );
 };
