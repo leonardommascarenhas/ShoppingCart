ba3bad77051dd6fb54276988edd9450b40f9d0c1
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-11-28 12:30:26 -0300

    list

--- src/Components/BuyProduct.js
+++ src/Components/BuyProduct.js
@@ -8,9 +8,6 @@ const BuyProduct = () => {
   const quantity = useRef(1);
 
   const buy = () => {
-    if (quantity.current.value <= 0) {
-      return alert("A quantidade escolhida deve ser maior que 0");
-    }
     return alert(`você comprou ${quantity.current.value} unidades de tamanho ${size.current.value}`);
   };
 
@@ -22,7 +19,7 @@ const BuyProduct = () => {
 
   return (
     <div className="grid grid-areas-slim grid-cols-slim grid-rows-slim lg:grid-areas-layout lg:grid-cols-layout lg:grid-rows-layout min-h-[80vh]">
-      <img src={clickedCloth[0].imageSrc} className="grid-in-[product] h-full "></img>
+      <img src={clickedCloth[0].imageSrc} className="grid-in-[product] h-full"></img>
       <h1 className="grid-in-[name] text-lg font-weight-700">{clickedCloth[0].name}</h1>
       <section className="grid-in-[size] flex flex-col">
         <h2 className="grid-in-[price] ">R$ {clickedCloth[0].price}</h2>
@@ -41,7 +38,17 @@ const BuyProduct = () => {
             <input type="number" name="quantity" id="quantity" ref={quantity} min="1" required />
             <button className="width">Comprar</button>
           </div>
-          <aside className=" grid-in-[aside]"></aside>
+          <aside className=" grid-in-[aside]">
+            A Sunt incididunt Lorem nostrud velit qui anim id. Est aute cupidatat nostrud esse. Laborum duis
+            exercitation sunt amet duis ipsum ea esse dolor. Irure minim veniam id enim deserunt aliquip ipsum
+            officia eiusmod fugiat in.
+            <ul className="list-disc">
+              <li>Exercitation deserunt cupidatat laboris pariatur eu voluptate proident enim.</li>
+              <li>Eu dolore consectetur culpa ipsum proident non minim commodo culpa duis nisi.</li>
+              <li>Labore laborum pariatur est reprehenderit.</li>
+              <li>Reprehenderit reprehenderit aliqua laborum est id commodo commodo do anim est</li>
+            </ul>
+          </aside>
         </form>
       </section>
     </div>
